9350b13a4afd84a930fe99bce7406799b6f48faa
Trelov Odracir <trelovodracir@gmail.com>
2024-12-13 12:18:37 -0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/RepoBuddy/', // substitua 'nome-do-repositorio' pelo nome real do seu repositório
   plugins: [react()],
 });
